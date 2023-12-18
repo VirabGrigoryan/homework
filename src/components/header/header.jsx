5f8f77b2e5526ac3f0de35a389d30fffc80dc8c6
VirabGrigoryan <vir.grig22@gmail.com>
2023-12-18 18:39:13 +0400

    react-project

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,35 +5,37 @@ import './style.css'
 class Header extends React.Component {
 
     render() {
-        return <header>
-            <div className={'G-container'}>
-                <div className={'header'}>
-                    <div className={'header-box'}>
-                         <a href="#">Free CSS Layouts</a>
-                        
-                    </div>
-                    <div className={'second-part'}>
+        return (
+            <header>
+                <div className={'G-container'}>
+                    <div className={'header'}>
+                        <div className={'header-box'}>
+                            <a href="#">Free CSS Layouts</a>
 
-                        <div className={'left'}>
-                            <p>Secondary Column
-                            </p>
                         </div>
-                        <div className={'centre same'}>
-                            <p>Content Here
-                            </p>
-                        </div>
-                        <div className={'right same'}>
-                            <p>Content Here
-                            </p>
-                        </div>
+                        <div className={'second-part'}>
 
-                    </div>
-                    <div className={'footer'}>
-                        <p>Footer</p>
+                            <div className={'dzax'}>
+                                <p>Secondary Column
+                                </p>
+                            </div>
+                            <div className={' nuyn'}>
+                                <p>Content Here
+                                </p>
+                            </div>
+                            <div className={'nuyn'}>
+                                <p>Content Here
+                                </p>
+                            </div>
+
+                        </div>
+                        <div className={'footer'}>
+                            <p>Footer</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        )
     }
 
 }
